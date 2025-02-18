37f2852b047b341cd8ab0e2153743815854ee93a
Dariya Hussein Kondapalli <dariya.k007@gmail.com>
2025-02-18 19:24:30 +0530

    urls

--- API/Api.js
+++ API/Api.js
@@ -2,5 +2,5 @@
 export const API_URL='https://swiggy-nm3z.onrender.com/'
 
 // export const USR_URL="http://localhost:3002/"
-export const USR_URL="https://foodappbackend-y99m.onrender.com"
+export const USR_URL="https://foodappbackend-y99m.onrender.com/"
 
