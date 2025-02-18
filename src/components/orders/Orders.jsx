d10c776834b1b622caa78b8dfa72f423553d849f
Dariya Hussein Kondapalli <dariya.k007@gmail.com>
2025-02-18 19:33:12 +0530

    path cahnged for images in the orders page

--- src/components/orders/Orders.jsx
+++ src/components/orders/Orders.jsx
@@ -94,7 +94,7 @@ const Orders = () => {
                       <div key={index} >
                         <div className="productsSection">
                           <div className="prdouctImage">
-                            <img src={`${items.image}`} />
+                            <img src={`${API_URL}/uploads/${items.image}`} />
                           </div>
                           <div className="details">
                             <div>
