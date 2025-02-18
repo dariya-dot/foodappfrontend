e19d0e4ff02ce68c258d9a50f88299495b914ab8
Dariya Hussein Kondapalli <dariya.k007@gmail.com>
2025-02-18 20:04:41 +0530

    products path changed in order.jsx

--- src/components/orders/Orders.jsx
+++ src/components/orders/Orders.jsx
@@ -94,7 +94,7 @@ const Orders = () => {
                       <div key={index} >
                         <div className="productsSection">
                           <div className="prdouctImage">
-                            <img src={`${API_URL}/uploads/${items.image}`} />
+                            <img src={`${API_URL}product/uploads/${items.image}`} />
                           </div>
                           <div className="details">
                             <div>
